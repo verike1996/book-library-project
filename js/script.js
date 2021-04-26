523aa93817a23adc72aadea324a0f65d66dee5df
Billy King <billyjack30@gmail.com>
2021-04-26 09:39:27 -0400

    added a function that pushes a new book object into a book library array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
-function Book(title, author, pages, readYet) {
+function Book(title, author, pages, progress) {
     this.title = title
     this.author = author
     this.pages = pages
-    this.readYet = readYet
+    this.progress = progress
+    // this.readYet = readYet
     this.info = function() {
         return`${this.title} by ${this.author}, ${this.pages} pages, ${this.readYet}`;
     }
 }
 
-// function getBook() {
-//     bookTitle = 
-// }
+function getBook() {
+    bookTitle = titleInput.value;
+    bookAuthor = authorInput.value;
+    bookPages = pagesInput.value;
+    bookProgress = progressInput.value;
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookProgress)
+    bookList.push(newBook);
+}
+
+let titleInput = document.getElementById('book-title');
+let authorInput = document.getElementById('book-author');
+let pagesInput = document.getElementById('book-pages');
+let progressInput = document.getElementById('book-progress');
+
+let bookTitle = null;
+let bookAuthor = null;
+let bookPages = 0;
+let bookProgress = 0;
+
+let bookList = [];
+
